0b46a350beb309ddcadfc8f5c1a9e8199e07b96c
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-06-10 03:52:13 +0200

    Restructure ex01 files

---
+++ ex01/include/ScavTrap.h
@@ -0,0 +1,27 @@
+// Author: vcordeir <vcordeir@student.42sp.org.br>
+// 42 SP
+
+#ifndef SCAVTRAP_H
+# define SCAVTRAP_H
+
+# include "ClapTrap.h"
+
+class ScavTrap : public ClapTrap
+{
+public:
+
+    ScavTrap( void );
+    ScavTrap( std::string pName );
+    ScavTrap( const ScavTrap& prScavTrap );
+    ~ScavTrap( void );
+
+    ScavTrap& operator=( const ScavTrap& prScavTrap );
+
+    void attack( const std::string& pTarget );
+    void guardGate( void );
+
+private:
+	std::string getClassName( void ) const;
+};
+
+#endif
